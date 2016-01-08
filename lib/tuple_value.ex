c3a6a42b5d4cae4f41081bab0b69243600736f52
Daryl Richter <daryl@revzilla.com>
2016-01-08 16:11:40 -0500

    rename tuple -> tuple_value
    that didn't take long. looks like this is the better way to go.

---
+++ lib/tuple_value.ex
@@ -0,0 +1,27 @@
+defmodule TupleValue do
+
+  @typedoc """
+    a tuple value (tuple for short) over those types—t is a set of n ordered triples of the form <Ai,Ti,vi>, 
+    where 
+      Ai is an attribute name,
+      Ti is a type name, and
+      vi is a value of type Ti.
+  """
+  @type tuple_value :: {atom, any}
+  
+  @spec new(atom, any) :: tuple_value
+  def new(atom, any) do
+    {atom, any}
+  end
+
+  @spec attribute(tuple_value) :: atom
+  def attribute(tuple_value) do
+    elem(tuple_value, 0)
+  end
+
+  @spec value(tuple_value) :: atom
+  def value(tuple_value) do
+    elem(tuple_value, 1)
+  end
+
+end
