b5f3a206507e4e237dc86648af4fa0bffee3e509
Daryl Richter <daryl@revzilla.com>
2016-01-18 11:31:03 -0500

    begin incorporating type into tuple_value
    Here I am already deviating from Dee (for example) and trying to stay
    closer to the "pure" implementation of D. Not sure how this is going to
    play out within Elixir since it doesn't really have the notion of
    explicit types.

--- lib/deejay/tuple_value.ex
+++ lib/deejay/tuple_value.ex
@@ -11,11 +11,11 @@ defmodule DeeJay.TupleValue do
       Ti is a type name, and
       vi is a value of type Ti.
   """
-  @type tuple_value :: {atom, any}
+  @type tuple_value :: {atom, atom, any}
   
   @spec tuple_value(atom, any) :: tuple_value
   def tuple_value(atom, any) do
-    {atom, any}
+    {atom, _typeof(any), any}
   end
 
   @spec attribute(tuple_value) :: atom
@@ -23,9 +23,24 @@ defmodule DeeJay.TupleValue do
     elem(tuple_value, 0)
   end
 
+  @spec type(tuple_value) :: atom
+  def type(tuple_value) do
+    elem(tuple_value, 1)
+  end
+
   @spec value(tuple_value) :: atom
   def value(tuple_value) do
-    elem(tuple_value, 1)
+    elem(tuple_value, 2)
+  end
+
+  @spec _typeof(any) :: atom
+  def _typeof(value) do
+    cond do
+      is_bitstring(value) -> :string
+      is_boolean(value)   -> :boolean
+      is_float(value)     -> :float
+      is_integer(value)   -> :integer
+    end
   end
 
 end
--- test/deejay/tuple_value_test.exs
+++ test/deejay/tuple_value_test.exs
@@ -4,18 +4,31 @@ defmodule DeeJayTupleValueTest do
   
   import DeeJay.TupleValue
   
-  test "Can return a normal Elixir tuple" do
-    assert tuple_value(:s_id, 1) == {:s_id, 1}
+  test "Is represented as a 3-values normal Elixir tuple" do
+    assert tuple_value(:s_id, 1) == {:s_id, :integer, 1}
   end
   
   test "The first element is called the attribute" do
     assert attribute(tuple_value(:s_id, 1)) == :s_id
-    assert attribute({:s_id, 1}) == :s_id
+  end
+  
+  test "The first element is called the attribute, passing a 3-value tuple" do
+    assert attribute({:s_id, :integer, 1}) == :s_id
   end
   
   test "The second element is called the value" do
     assert value(tuple_value(:s_id, 1)) == 1
-    assert value({:s_id, 1}) == 1
+  end
+  
+  test "The second element is called the value, passing a 3-value tuple" do
+    assert value({:s_id, :integer, 1}) == 1
+  end
+  
+  test "The type is inferred from the value" do
+    assert type(tuple_value(:s_id, 1))       == :integer
+    assert type(tuple_value(:s_id, 1.1))     == :float
+    assert type(tuple_value(:s_id, "Magic")) == :string
+    assert type(tuple_value(:s_id, true))    == :boolean
   end
   
 end
