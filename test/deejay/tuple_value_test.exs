ce32449c3ba58675796ccad92be347029de280f7
Daryl Richter <daryl@revzilla.com>
2016-02-05 09:06:21 -0500

    Some test refinements. Clearly, I don't understand Typespecs and behaviours.
    YET!

--- test/deejay/tuple_value_test.exs
+++ test/deejay/tuple_value_test.exs
@@ -4,26 +4,35 @@ defmodule DeeJayTupleValueTest do
   
   import DeeJay.TupleValue
   
-  test "Is represented as a 3-values normal Elixir tuple" do
+  test "Is represented as a 3-valued normal Elixir tuple" do
     assert tuple_value(:s_id, 1) == {:s_id, :integer, 1}
   end
   
-  test "The first element is called the attribute" do
-    assert attribute(tuple_value(:s_id, 1)) == :s_id
+  describe "The first element is called the attribute" do
+    test "attribute created using tuple_value()" do
+      assert attribute(tuple_value(:s_id, 1)) == :s_id
+    end
+  
+    test "attribute created using a valid 3-value tuple" do
+      assert attribute({:s_id, :integer, 1}) == :s_id
+    end
+
+    # I am not sure why this passes right now, shouldn't the spec prevent it.
+    test "returns an error if tuple is constructed incorrectly" do
+      assert attribute({1, 1, :integer}) == 1
+    end
   end
   
-  test "The first element is called the attribute, passing a 3-value tuple" do
-    assert attribute({:s_id, :integer, 1}) == :s_id
-  end
+  describe "The second element is called the value" do
+    test "value created using tuple_value()" do
+      assert value(tuple_value(:s_id, 1)) == 1
+    end
   
-  test "The second element is called the value" do
-    assert value(tuple_value(:s_id, 1)) == 1
+    test "value created using a valid 3-value tuple" do
+      assert value({:s_id, :integer, 1}) == 1
+    end
   end
-  
-  test "The second element is called the value, passing a 3-value tuple" do
-    assert value({:s_id, :integer, 1}) == 1
-  end
-  
+    
   describe "The type is inferred from the value" do
     it "recognizes an integer" do
       assert type(tuple_value(:s_id, 1)) == :integer
